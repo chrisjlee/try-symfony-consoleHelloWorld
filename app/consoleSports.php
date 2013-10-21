0f661941590b548df9a958e77a60aefffdc58372
Chris Lee <iam@chrisjlee.net>
2013-10-20 21:49:02 -0500

    Update consolesports comments

--- app/consoleSports.php
+++ app/consoleSports.php
@@ -9,25 +9,25 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 // use Symfony\Component\DomCrawler\Crawler;
 
-$console = new Application('ESPN', '0.1.0');
+$console = new Application('Console Sports', '0.1.0');
 /**
  *  Console: Using the console perform an API request to api.espn.com
  */
 
-$key = '99adppxa2pkmyrb537shwptw';
-$uri = 'http://api.espn.com/v1/sports?apikey=';
-$resource = $key . $uri;
-
-$request = new Buzz\Message\Request('HEAD', '/', $resource);
-
 $console
-  ->register('ESPN:run')
-  ->setDescription('.')
+  ->register('sports:run')
+  ->setDescription('Example application')
+  ->setHelp('
+The <info>sports:run</info> command will print out your argument if it is on the
+whitelist of valid sports type.
+')
   ->setDefinition(array(
     new InputArgument('sportType', InputArgument::OPTIONAL, 'Please provide a sport type', 'baseball'),
   ))
   ->setCode(function (InputInterface $input, OutputInterface $output) {
 
+    // Only allow the following arguments to validate. Otherwise produce
+    // an error message.
     $sportTypeWhitelist = array(
       'baseball',
       'basketball',
@@ -40,7 +40,7 @@ $console
     if (!in_array($sportType, $sportTypeWhitelist)) {
       $output->writeln('<error>Please provide a valid sport type.</error>');
     } else {
-        $output->writeln($sportType);
+        $output->writeln('<info>' . $sportType . '</info>');
     }
   });
 
